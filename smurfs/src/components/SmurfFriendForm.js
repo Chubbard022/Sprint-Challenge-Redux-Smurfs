f923c08a247ad2ff4ddec31078a2fdf95dd9d951
Chubbard022 <chubbard022@gmail.com>
2019-04-12 09:11:37 -0700

    added smurfForm and list to components, rendering dummy text

---
+++ smurfs/src/components/SmurfFriendForm.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class SmurfFriendForm extends React.Component{
+    state = {
+        name:'',
+        age:'',
+        height:''
+    }
+    render(){
+        return(
+            <div>
+                hi
+            </div>
+        )
+    }
+}
+
+
+
+export default SmurfFriendForm;
---
+++ smurfs/src/components/SmurfFriendList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SmurfFriendList extends React.Component{
+    render(){
+        return(
+            <div>
+                hi
+            </div>
+        )
+    }
+}
+
+export default SmurfFriendList;
