1112bd02e603fa7caf0364b337c610dac0f565c9
Chubbard022 <chubbard022@gmail.com>
2019-04-12 08:56:12 -0700

    finished adding actions both getting and adding smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,35 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
+export const GET_SMURF_START = "GET_SMURF_START";
+export const GET_SMURF_SUCCESS = "GET_SMURF_SUCCESS";
+export const GET_SMURF_FAILURE = "GET_SMURF_FAILURE";
+
+export const ADD_SMURF_START = "ADD_SMURF_START";
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS";
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
+
+const URL = "localhost:3333/smurfs"
+
+
+export const getSmurfs = () => dispatch =>{
+  dispatch({type:GET_SMURF_START})
+  return axios
+  .get(URL)
+  .then(res=>{
+    dispatch({type:GET_SMURF_SUCCESS, payload: res.data})
+  })
+  .catch(err=>console.log(err))
+}
+
+export const addSmurf = smurf => dispatch =>{
+  dispatch({type:ADD_SMURF_START})
+  return axios
+  .post(URL)
+  .then(res=>{
+    dispatch({type:ADD_SMURF_SUCCESS, payload: res.data})
+  })
+  .then(err=>console.log(err))
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
