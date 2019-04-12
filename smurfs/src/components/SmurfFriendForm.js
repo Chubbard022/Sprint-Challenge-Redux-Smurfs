e8f0c0657c27ebb824c0d3fc939ea16226f5d5e6
Chubbard022 <chubbard022@gmail.com>
2019-04-12 09:27:31 -0700

    input form working, not passing props to it yet

--- smurfs/src/components/SmurfFriendForm.js
+++ smurfs/src/components/SmurfFriendForm.js
@@ -6,11 +6,41 @@ class SmurfFriendForm extends React.Component{
         age:'',
         height:''
     }
+    handleChange = event =>{
+        this.setState({[event.target.name]: event.target.value})
+    }
+    handleSubmit = event =>{
+        event.preventDefault();
+        // this.props.addsmurf({name:this.state.name,age:this.state.age,height:this.state.height})
+        this.setState({name:'',age:'',height:''})
+        
+    }
     render(){
         return(
-            <div>
-                hi
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="number"
+                    name="age"
+                    placeholder="age"
+                    value={this.state.age}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="height"
+                    placeholder="height"
+                    value={this.state.height}
+                    onChange={this.handleChange}
+                />
+                <button>Sumbit</button>
+            </form>
         )
     }
 }
