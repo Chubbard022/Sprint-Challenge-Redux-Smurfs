7ffb8cd8b861e4468907d9ff783c5b8f40b41f09
Chubbard022 <chubbard022@gmail.com>
2019-04-12 09:04:45 -0700

    finished reducer switch statements

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,66 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  GET_SMURF_START,
+  GET_SMURF_SUCCESS,
+  GET_SMURF_FAILURE,
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE
+} from '../actions'
 
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+}
+
+const reducer = (state=initialState, action) =>{
+  switch(action.type){
+    case GET_SMURF_START:
+      return{
+        ...state,
+        error: '',
+        fetchingSmurfs: true
+      }
+    case GET_SMURF_SUCCESS:
+      return{
+        ...state,
+        error: null,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+
+      }
+    case GET_SMURF_FAILURE:
+      return{
+        ...state,
+        error:action.payload
+      }
+    case ADD_SMURF_START:
+      return{
+        ...state,
+        error:'',
+        addingSmurf: true,
+      }
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        error: null,
+        addingSmurf:false,
+
+      }
+    case ADD_SMURF_FAILURE:
+      return{
+        ...state,
+        error:action.payload
+      }
+    default:
+    return state;
+  }
+}
+export default reducer;
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
